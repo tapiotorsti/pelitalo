0df506688a4770b02518538554768b15c190ebec
Torsti Tapiola <torsti.tapiola@edu.sasky.fi>
2021-09-14 08:18:24 +0300

    Sivupohjat lisätty

--- public/index.php
+++ public/index.php
@@ -9,14 +9,18 @@ require_once '../src/init.php';
 $request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
 $request = strtok($request, '?');
 
-//Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
-// käsittelijä
-if ($request === '/' || $request === '/tapahtumat') {
-    echo '<h1>Kaikki tapahtumat</h1>';
-}   else if ($request === '/tapahtuma') {
-    echo '<h1>Yksittäisen tapahtuman tiedot</h1>';
-}   else {
-    echo '<h1>Pyydettyä sivua ei löytynyt :(</h1>';
-}
+  // Luodaan uusi Plates-olio ja kytketään se sovelluksen sivupohjiin.
+  $templates = new League\Plates\Engine('../src/view');
+
+  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
+  // käsittelijä.
+  if ($request === '/' || $request === '/tapahtumat') {
+    echo $templates->render('tapahtumat');
+  } else if ($request === '/tapahtuma') {
+    echo $templates->render('tapahtuma');
+  } else {
+    echo $templates->render('notfound');
+  }
+
 
 ?>
--- src/init.php
+++ src/init.php
@@ -2,4 +2,6 @@
 
   require_once '../config/config.php';
 
+  require_once '../vendor/autoload.php';
+
 ?>
