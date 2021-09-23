d313dc3382b0d400ddfd16827360afe18e594d6d
Torsti Tapiola <torsti.tapiola@edu.sasky.fi>
2021-09-23 09:42:24 +0300

    tietokantakäsittelyn kääreluokka

--- config/config.php
+++ config/config.php
@@ -1,10 +1,17 @@
 <?php
 
-  $config = array(
-    "urls" => array(
+$config = array(
+  "db" => array(
+     "dbname" => $_SERVER["DB_DATABASE"],
+     "username" => $_SERVER["DB_USERNAME"],
+     "password" => $_SERVER["DB_PASSWORD"],
+     "host" => "localhost"
+  ),
+  "urls" => array(
       "baseUrl" => "/~ttapiola/lanify"
-    )
-  );
+  )
+);
+
 
   define("PROJECT_ROOT", dirname(__DIR__) . "/");
   define("HELPERS_DIR", PROJECT_ROOT . "src/helpers/");
