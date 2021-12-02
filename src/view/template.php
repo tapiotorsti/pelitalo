895ebeb91177dddf9cf9df09abb9ef2f914abe22
Torsti Tapiola <torsti.tapiola@edu.sasky.fi>
2021-12-02 03:52:28 +0200

    kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -8,7 +8,17 @@
   <body>
   <header>
       <h1><a href="<?=BASEURL?>">lanify</a></h1>
-  </header>
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
+    </header>
     <section>
       <?=$this->section('content')?>
     </section>
