ab3a66feee92f7e0c01399e6ae63684b8c00f1a0
Torsti Tapiola <torsti.tapiola@edu.sasky.fi>
2021-12-02 10:24:57 +0200

    ilmoittautumistietojen välitys sivupohjalle

--- public/index.php
+++ public/index.php
@@ -29,20 +29,23 @@
   // käsittelijä.
   switch ($request) {
     case '/':
-    case '/tapahtumat':
-      require_once MODEL_DIR . 'tapahtuma.php';
-      $tapahtumat = haeTapahtumat();
-      echo $templates->render('tapahtumat',['tapahtumat' => $tapahtumat]);
-      break;
-    case '/tapahtuma':
-      require_once MODEL_DIR . 'tapahtuma.php';
-      $tapahtuma = haeTapahtuma($_GET['id']);
-      if ($tapahtuma) {
-        echo $templates->render('tapahtuma',['tapahtuma' => $tapahtuma]);
-      } else {
-        echo $templates->render('tapahtumanotfound');
-      }
-      break;
+      case '/tapahtuma':
+        require_once MODEL_DIR . 'tapahtuma.php';
+        require_once MODEL_DIR . 'ilmoittautuminen.php';
+        $tapahtuma = haeTapahtuma($_GET['id']);
+        if ($tapahtuma) {
+          if ($loggeduser) {
+            $ilmoittautuminen = haeIlmoittautuminen($loggeduser['idhenkilo'],$tapahtuma['idtapahtuma']);
+          } else {
+            $ilmoittautuminen = NULL;
+          }
+          echo $templates->render('tapahtuma',['tapahtuma' => $tapahtuma,
+                                               'ilmoittautuminen' => $ilmoittautuminen,
+                                               'loggeduser' => $loggeduser]);
+        } else {
+          echo $templates->render('tapahtumanotfound');
+        }
+        break;  
     case '/lisaa_tili':
       if (isset($_POST['laheta'])) {
         $formdata = cleanArrayData($_POST);
